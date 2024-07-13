8f9d049b52e366796b847fea7cab90b58584285f
Ben Barlow <ben-j-barlow.1@gmail.com>
2024-07-13 09:48:17 +0100

    sandbox: add upscaling of ore map

--- sandbox/initial_plots.jl
+++ sandbox/initial_plots.jl
@@ -29,14 +29,17 @@ function generate_new_ore()
     up = MEBeliefUpdater(m, 1000, 2.0)
     b0 = POMDPs.initialize_belief(up, ds0)
 
+    @show size(s0.ore_map)
 
     cmap=:viridis
     verbose=true
     ore_fig = plot_ore_map(s0.ore_map, cmap)
-
+    ore_fig
+    return s0.ore_map
 end
 
-generate_new_ore()
+s0_ore_map = generate_new_ore()
+
 
 mass_fig, r_massive = plot_mass_map(s0.ore_map, m.massive_threshold, cmap; truth=true)
 b0_fig = plot(b0; cmap=cmap)
@@ -46,3 +49,37 @@ display(ore_fig)
 display(mass_fig)
 display(b0_fig)
 display(b0_hist)
+
+
+
+using Interpolations
+using ImageFiltering
+
+# Remove the third dimension since it is 1
+s0_ore_map_2d = reshape(s0_ore_map, 50, 50)
+
+# Increase the resolution using interpolation
+interpolated_map = interpolate(s0_ore_map_2d, BSpline(Linear()))
+
+# factor for upscale
+factor = 10
+
+# Define the new dimensions for the higher resolution grid
+new_dims = (size(s0_ore_map_2d, 1) * factor, size(s0_ore_map_2d, 2) * factor)
+
+# Generate the high resolution grid points
+high_res_x = range(1, stop=size(s0_ore_map_2d, 1), length=new_dims[1])
+high_res_y = range(1, stop=size(s0_ore_map_2d, 2), length=new_dims[2])
+
+# Create a high resolution image by evaluating the interpolation at the new grid points
+high_res_map_array = [interpolated_map(x, y) for x in high_res_x, y in high_res_y]
+
+# Apply Gaussian filter to smooth the image
+sigma = 1.0  # You can adjust the sigma value for more or less smoothing
+smoothed_map = imfilter(high_res_map_array, Kernel.gaussian(sigma))
+
+cmap=:viridis
+plot_ore_map(s0_ore_map, cmap, "my_title")
+
+
+MineralExploration.plot_ore_map
