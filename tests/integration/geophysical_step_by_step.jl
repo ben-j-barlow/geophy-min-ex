5569ccbfde49856d9bc249d2875b41fb4274c11c
Ben Barlow <ben-j-barlow.1@gmail.com>
2024-08-05 18:53:02 +0100

    tests: add custom path step by step test

--- tests/integration/geophysical_step_by_step.jl
+++ tests/integration/geophysical_step_by_step.jl
@@ -3,52 +3,35 @@ using POMDPs
 using Plots
 using MineralExploration
 using POMDPModelTools
+using Random
 
-function pad_line(description::String, original::String, current::String, pad_length::Int)
-    pad = " " ^ (pad_length - length(description))
-    println(description * pad * " | " * original * " | " * current)
-end
+include("helpers.jl")
 
-function mylog(current::MEBelief{GeoStatsDistribution}, original::MEBelief{GeoStatsDistribution})
-    #println("Beliefs equal: $(isequal(current, original))")
-    #println("Mismatched properties: " * join([p for p in propertynames(current) if !isequal(getproperty(current, p), getproperty(original, p))], ", "))
-    pad_line("obs base map coords", string(original.geophysical_obs.base_map_coordinates), string(current.geophysical_obs.base_map_coordinates), 20)
-    pad_line("bank angle", string(original.agent_bank_angle), string(current.agent_bank_angle), 20)
-    println("geostats distributions are equal: $(isequal(current.geostats, original.geostats))")
-end
-
-function log(current::MEState, original::MEState)
-    pad_line("heading", string(original.agent_heading), string(current.agent_heading), 20)
-    pad_line("pos x", string(original.agent_pos_x), string(current.agent_pos_x), 20)
-    pad_line("pos y", string(original.agent_pos_y), string(current.agent_pos_y), 20)
-end
-
-C_EXP = 2
-
+C_EXP = 100
 N_PARTICLES = 1000
 NOISE_FOR_PERTURBATION = 2.0
 
 m = MineralExplorationPOMDP(
     c_exp=C_EXP,
     sigma=20,
-    init_heading=convert(Float64, 0),
-    init_pos_x=0,
-    init_pos_y=0,
-    velocity=40,
+    init_heading=HEAD_EAST,
+    init_pos_x=201,
+    init_pos_y=700,
+    velocity=25,
     out_of_bounds_cost=0.5,
     geophysical_noise_std_dev=convert(Float64, 0.0),
     observations_per_timestep=1,
     timestep_in_seconds=1
 )
 
-up = MEBeliefUpdater(m, N_PARTICLES, NOISE_FOR_PERTURBATION) #Checked
+up = MEBeliefUpdater(m, N_PARTICLES, NOISE_FOR_PERTURBATION)
 ds0 = POMDPs.initialstate(m)
 
-b0 = POMDPs.initialize_belief(up, ds0) #Checked
+b0 = POMDPs.initialize_belief(up, ds0);
+Random.seed!(42)
 s0 = rand(ds0);
-s0_copy = deepcopy(s0)
-b0_copy = deepcopy(b0)
-
+s0_copy = deepcopy(s0);
+b0_copy = deepcopy(b0);
 
 solver = POMCPOWSolver(
     tree_queries=2000,
@@ -66,50 +49,122 @@ solver = POMCPOWSolver(
     estimate_value=leaf_estimation,
     tree_in_info=false,
 )
-planner = POMDPs.solve(solver, m)
+planner = POMDPs.solve(solver, m);
 
 
 # action info : inspect b0 vs copied version
 
-a = MEAction(type=:fly, change_in_bank_angle=0)
-a, ai = action_info(planner, b0);
-a_copy = deepcopy(a)
+a = MEAction(type=:fly, change_in_bank_angle=0);
 
-#mylog(b0, b0_copy)
+b = b0;
+s = s0;
 
-#println("Mismatched properties: " * join([p for p in propertynames(b0) if !isequal(getproperty(b0, p), getproperty(b0_copy, p))], ", "))
-
-#p500_current = b0.particles[500];
-#p500_original = b0_copy.particles[500];
-#println("Mismatched properties: " * join([p for p in propertynames(p500_current) if !isequal(getproperty(p500_current, p), getproperty(p500_original, p))], ", "))
-
-#plot_map(b0.particles[500].ore_map, "particle 500 current")
-#plot_map(b0_copy.particles[500].ore_map, "particle 500 original")
-#plot_map(b0.particles[500].smooth_map, "particle 500 current")
-#plot_map(b0_copy.particles[500].smooth_map, "particle 500 original")
+r_massive = sum(s0.ore_map[:,:,1] .>= m.massive_threshold)
 
 
-# run gen : inspect s0 and a vs copied version
-out = gen(m, s0, a, m.rng);
-o = out[:o];
-s1 = out[:sp];
-r = out[:r];
+mass_fig, r_massive = plot_mass_map(s0.ore_map, m.massive_threshold, :viridis; truth=true)
+b0_hist, vols, mean_vols, std_vols = plot_volume(m, b0, r_massive; t=0, verbose=true)
+p = plot_map(s0.ore_map, "ore")
 
-o_copy = deepcopy(o);
-s1_copy = deepcopy(s1);
+display(mass_fig)
+display(p)
+display(b0_hist)
 
-println("Mismatched properties: " * join([p for p in propertynames(s0) if !isequal(getproperty(s0, p), getproperty(s0_copy, p))], ", "))
-s0.geophysical_obs
-#s0_copy.geophysical_obs
-# I HAVE IMPLEMENTED THE ISEQUAL FUNCTION FOR GeophysicalObservations BUT I HAVE NOT TESTED IT
-
-x, y = o.agent_pos_x, o.agent_pos_y;
-@assert last(s1.agent_pos_x) == x
-@assert last(s1.agent_pos_y) == y
+empty!(mass_fig)
+empty!(p)
+empty!(b0_hist)
 
 
-# run update : inspect b0, a, o vs copied version
-bp, ui = update_info(up, b0, a, o)
 
-plot(bp, m)
-x, y = get_agent_trajectory(bp.agent_bank_angle, m);
+for i in 1:30
+    out = gen(m, s, a, m.rng);
+    o = out[:o];
+    sp = out[:sp];
+    bp, ui = update_info(up, b, a, o);
+    
+    @info ""
+    @info "timestep $i"
+    @info "b geostats base map coords  $(b.geostats.geophysical_data.base_map_coordinates)"
+    @info "s norm (base) agent pos     x $(last(sp.agent_pos_x) / m.base_grid_element_length) y $(last(sp.agent_pos_y) / m.base_grid_element_length)"
+    @info "obs base map coords         $(o.geophysical_obs.base_map_coordinates)"
+    @info "s norm (smooth) agent pos   x $(last(sp.agent_pos_x) / m.smooth_grid_element_length) y $(last(sp.agent_pos_y) / m.smooth_grid_element_length)"
+    @info "obs smooth map coords       $(o.geophysical_obs.smooth_map_coordinates)"
+    @info "bp geostats base map coords $(bp.geostats.geophysical_data.base_map_coordinates)"
+    
+    if i % 4 == 0
+        p, _, _, _ = plot_volume(m, bp, r_massive, t=i)
+        display(p)
+        empty!(p)
+        
+        p = plot(bp, m, sp)
+        display(p)
+        empty!(p)
+
+        p = plot_smooth_map_and_plane_trajectory(sp, m)
+        display(p)
+        empty!(p)
+    end
+    
+    b = bp
+    s = sp
+end
+
+
+
+a = MEAction(type=:fly, change_in_bank_angle=10);
+for i in 1:4
+    out = gen(m, s, a, m.rng);
+    o = out[:o];
+    sp = out[:sp];
+    bp, ui = update_info(up, b, a, o);
+
+    s = sp
+    b = bp
+end
+
+a = MEAction(type=:fly, change_in_bank_angle=-10);
+for i in 1:5
+    out = gen(m, s, a, m.rng);
+    o = out[:o];
+    sp = out[:sp];
+    bp, ui = update_info(up, b, a, o);
+
+    s = sp
+    b = bp
+end
+
+
+t = 39
+a = MEAction(type=:fly, change_in_bank_angle=0);
+for i in t:(t+10)
+    out = gen(m, s, a, m.rng);
+    o = out[:o];
+    sp = out[:sp];
+    bp, ui = update_info(up, b, a, o);
+    
+    @info ""
+    @info "timestep $i"
+    @info "b geostats base map coords  $(b.geostats.geophysical_data.base_map_coordinates)"
+    @info "s norm (base) agent pos     x $(last(sp.agent_pos_x) / m.base_grid_element_length) y $(last(sp.agent_pos_y) / m.base_grid_element_length)"
+    @info "obs base map coords         $(o.geophysical_obs.base_map_coordinates)"
+    @info "s norm (smooth) agent pos   x $(last(sp.agent_pos_x) / m.smooth_grid_element_length) y $(last(sp.agent_pos_y) / m.smooth_grid_element_length)"
+    @info "obs smooth map coords       $(o.geophysical_obs.smooth_map_coordinates)"
+    @info "bp geostats base map coords $(bp.geostats.geophysical_data.base_map_coordinates)"
+    
+    if i % 4 == 0
+        p, _, _, _ = plot_volume(m, bp, r_massive, t=i)
+        display(p)
+        empty!(p)
+        
+        p = plot(bp, m, sp)
+        display(p)
+        empty!(p)
+
+        p = plot_smooth_map_and_plane_trajectory(sp, m)
+        display(p)
+        empty!(p)
+    end
+    
+    b = bp
+    s = sp
+end
