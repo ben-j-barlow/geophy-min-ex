f91ff5b6994fb61a0c20290f14d19b9c2fb02f26
Ben Barlow <ben-j-barlow.1@gmail.com>
2024-08-12 21:07:56 +0100

    trial: updates

--- scripts/run_geophysical_trial.jl
+++ scripts/run_geophysical_trial.jl
@@ -19,79 +19,73 @@ NOISE_FOR_PERTURBATION = 0.1
 C_EXP = 125
 GET_TREES = false
 
-SEED = 42
 
-SAVE_DIR = "./data/geophys_trial$(SEED)/" 
+SAVE_DIR = "./data/trials/" 
 
 
-if isdir(SAVE_DIR)
-    rm(SAVE_DIR; recursive=true)
+#if isdir(SAVE_DIR)
+#    rm(SAVE_DIR; recursive=true)
     #error("directory already exists")
-end 
+#end 
 
-mkdir(SAVE_DIR)
+#mkdir(SAVE_DIR)
 
 #io = MineralExploration.prepare_logger()
 
-grid_dims = (50, 50, 1)
-
 m = MineralExplorationPOMDP(
-    grid_dim=grid_dims,
-    c_exp=C_EXP,
-    sigma=5,
-    init_heading=HEAD_NORTH,
-    mainbody_gen=BlobNode(grid_dims=grid_dims),
-    true_mainbody_gen=BlobNode(grid_dims=grid_dims),
-    geophysical_noise_std_dev=0.0,
-    observations_per_timestep=1,
-    timestep_in_seconds=1,
-    init_pos_x=25 * 25,
-    init_pos_y=25 * 25,
-    bank_angle_intervals=15,
-    max_bank_angle=55,
-    velocity=25,
-    base_grid_element_length=25.0
+    grid_dim = (50, 50, 1),
+    c_exp = 100.0,
+    base_grid_element_length = 40.0,
+    upscale_factor = 4,
+    sigma = 5,
+    geophysical_noise_std_dev = 0.01,
+    fly_cost = 0.01,
+    out_of_bounds_cost = 0.0,
+    out_of_bounds_tolerance = 0,
+    init_pos_x = 100.0,
+    init_pos_y = 0.0,
+    init_heading = HEAD_NORTH,
+    max_bank_angle = 55,
+    bank_angle_intervals = 15,
+    velocity = 50,
+    extraction_cost = 160.0,
+    extraction_lcb = 0.7,
+    extraction_ucb = 0.7
 )
 
 # do not call
 #initialize_data!(m, N_INITIAL)
 
-ds0 = POMDPs.initialstate(m)
-
-Random.seed!(SEED)
-s0 = rand(ds0; truth=true) #Checked
-
-# get min amd max value of ore map
-min_ore = minimum(s0.ore_map)
-max_ore = maximum(s0.ore_map)
-minimum(s0.smooth_map)
-maximum(s0.smooth_map)
-
-up = MEBeliefUpdater(m, N_PARTICLES, NOISE_FOR_PERTURBATION) #Checked
-b0 = POMDPs.initialize_belief(up, ds0) #Checked
-
-solver = POMCPOWSolver(
-    tree_queries=50000,
-    k_observation=2.0,
-    alpha_observation=0.1,
-    max_depth=7,
-    check_repeat_obs=false,
-    check_repeat_act=true,
-    enable_action_pw=false,
-    #next_action=nothing,
-    #alpha_action=nothing,
-    #k_action=nothing,
-    criterion=POMCPOW.MaxUCB(m.c_exp),
-    final_criterion=POMCPOW.MaxQ(),
-    estimate_value=leaf_estimation,
-    tree_in_info=false,
-)
+solver = get_geophysical_solver(m.c_exp)
 planner = POMDPs.solve(solver, m)
 
-discounted_return, n_flys, final_belief, final_state, trees = run_geophysical_trial(m, up, planner, s0, b0, max_t=100, output_t=5, save_dir=SAVE_DIR, return_all_trees=GET_TREES);
+b = nothing
+trees = nothing
+final_belief = nothing
 
-plot_base_map_and_plane_trajectory(final_state, m)
+ds0 = POMDPs.initialstate(m)
 
+SEED = 42 # convert(Int64, floor(rand() * 10000))
+@info "$(SEED)"
+Random.seed!(SEED)
+
+for i in 1:N
+s0 = rand(ds0)
+r_massive = sum(s0.ore_map .>= m.massive_threshold)
+up = MEBeliefUpdater(m, N_PARTICLES, NOISE_FOR_PERTURBATION)
+b0 = POMDPs.initialize_belief(up, ds0)
+
+discounted_return, n_flys, final_belief, final_state, trees = run_geophysical_trial(m, up, planner, s0, b0, max_t=110, output_t=5, return_all_trees=GET_TREES, display_figs=false);
+
+p = plot_base_map_and_plane_trajectory(final_state, m, t=n_flys)
+
+#b_hist, vols, mn, std = plot_volume(m, bp, r_massive; t=t, verbose=false)
+#title!(p, "$(final_belief.acts[end].type) at t=$(n_flys+1) with belief $mn ± $std & truth $r_massive")
+#savefig(p, "$(SAVE_DIR)trial_$(seed).png")
+
+#p = plot_base_map_and_plane_trajectory(final_state, m, t=n_flys)
+
+#final_belief.acts
 #for i in 1:3
 #    inbrowser(D3Tree(trees[i], init_expand=1), "safari")
 #    @info ""
