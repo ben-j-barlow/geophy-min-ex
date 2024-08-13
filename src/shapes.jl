a016dcc14dab2497788678a5e8d12d30e561a485
Ben Barlow <ben-j-barlow.1@gmail.com>
2024-08-13 11:12:31 +0100

    state: return to using centring of ore

--- src/shapes.jl
+++ src/shapes.jl
@@ -59,7 +59,7 @@ end
 """
 Distribution for determining the center of the mainbody.
 """
-function center_distribution(grid_dims; bounds=[grid_dims[1]/10, 9*grid_dims[1]/10])
+function center_distribution(grid_dims; bounds=[grid_dims[1]/4, 3*grid_dims[1]/4])
     xdistr = Distributions.Uniform(bounds[1], bounds[2])
     ydistr = Distributions.Uniform(bounds[1], bounds[2])
     return Product([xdistr, ydistr])
