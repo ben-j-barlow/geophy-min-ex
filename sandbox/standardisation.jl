fdd72fbc5e10e830abcdce28f943c0def5da0729
Ben Barlow <ben-j-barlow.1@gmail.com>
2024-08-25 22:04:13 +0100

    understanding

--- sandbox/standardisation.jl
+++ sandbox/standardisation.jl
@@ -6,9 +6,45 @@ using Statistics
 seeds = get_all_seeds()
 GRID_DIM = (48,48,1)
 
+
+MAIN_MAP_VOL = Float64[]
+MAIN_TH = 0.46
+r_masive = nothing
+states = []
+
+for seed in seeds[1:150]
+    Random.seed!(seed)
+    m = MineralExplorationPOMDP(
+        grid_dim=GRID_DIM,
+        massive_threshold=0.48
+    )
+    ds0 = POMDPs.initialstate(m)
+    s0 = rand(ds0, apply_scale=false)
+    push!(states, s0)
+end
+
+
+function get_cnt(ls, th, cost)
+    s_mass = [ele.mainbody_map .> th for ele in ls]
+    r_mass = [sum(ele) for ele in s_mass]
+    println(sum([ele < cost for ele in r_mass]))
+end
+
+get_cnt(states, 0.45, 155)
+
+
+using Plots
+BINS = 0:25:350
+#histogram(ORE_MAP_VOL, label="Ore Map", bins = BINS)
+histogram(MAIN_MAP_VOL, label="Main Body Map", bins = BINS)
+
+length(MAIN_MAP_VOL)
+MAIN_COUNT = sum(MAIN_MAP_VOL .> 150)
+
+
 ORE_MAP_VOL = Float64[]
 ORE_TH = 0.7
-for seed in seeds[1:1]
+for seed in seeds[1:150]
 
     Random.seed!(seed)
     m = MineralExplorationPOMDP(
@@ -21,34 +57,13 @@ for seed in seeds[1:1]
     s_massive = s0.ore_map .>= ORE_TH
     r_massive = sum(s_massive)
     push!(ORE_MAP_VOL, r_massive)
-
-    display(plot_map(s0.mainbody_map,""))
-    display(plot_map(s0.ore_map,""))
-    
-    display(plot_map(s0.smooth_map,""))
 end
 
 
-MAIN_MAP_VOL = Float64[]
-MAIN_TH = 0.46
-for seed in seeds[1:150]
-    Random.seed!(seed)
-    m = MineralExplorationPOMDP(
-        grid_dim=GRID_DIM,
-    )
-    ds0 = POMDPs.initialstate(m)
-    s0 = rand(ds0, apply_scale=false)
 
-    s_massive = s0.mainbody_map .>= MAIN_TH
-    r_massive = sum(s_massive)
-    push!(MAIN_MAP_VOL, r_massive) 
-end
 
 # plot a histogram of the each list, narrow bin width
-using Plots
-BINS = 0:25:350
-histogram(ORE_MAP_VOL, label="Ore Map", bins = BINS)
-histogram(MAIN_MAP_VOL, label="Main Body Map", bins = BINS)
+
 
 # count # > 150
 length(ORE_MAP_VOL)
